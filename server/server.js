4f9ed648822d3af3012ed2387c9efc569d6e79d9
yupolischuk <yumuscletank@gmail.com>
2018-11-29 16:54:37 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+
 
 var app = express();
 
@@ -29,6 +32,28 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+
+// GET /todos/1234567
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400);
+    });
+
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
