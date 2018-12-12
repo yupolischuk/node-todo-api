1c7efa9a994cb65d453badc34ce08e4e8d21806f
yupolischuk <yumuscletank@gmail.com>
2018-12-12 18:00:25 +0200

    Made todo routes private

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -7,42 +7,44 @@ const {User} = require('./../../models/user');
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 const users = [{
-  _id: userOneId,
-  email: 'yura@example.com',
-  password: 'userOnePass',
-  tokens: [{
-    access: 'auth',
-    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
-  }]
+    _id: userOneId,
+    email: 'yura@example.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
 }, {
-  _id: userTwoId,
-  email: 'jen@example.com',
-  password: 'userTwoPass'
+    _id: userTwoId,
+    email: 'jen@example.com',
+    password: 'userTwoPass'
 }];
 
 const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
+    _id: new ObjectID(),
+    text: 'First test todo',
+    _creator: userOneId
 }, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 };
 
 const populateUsers = (done) => {
-  User.remove({}).then(() => {
-    var userOne = new User(users[0]).save();
-    var userTwo = new User(users[1]).save();
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
 
-    return Promise.all([userOne, userTwo])
-  }).then(() => done());
+        return Promise.all([userOne, userTwo])
+    }).then(() => done());
 };
 
 module.exports = {todos, populateTodos, users, populateUsers};
